cc1b45cff1fa89cf41475a2358d915acf24c153c
Abraham <abrahamjms156@gmail.com>
2023-06-20 12:47:45 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
-
 int _putchar(char c);
 void print_alphabeth(void);
 void print_alphabeth_x10(void);
@@ -11,9 +10,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int,int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
-
 #endif
