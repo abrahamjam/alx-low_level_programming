89c9cfdef53e3b3858aae40678221db560b821a9
Abraham <abrahamjms156@gmail.com>
2023-06-21 08:53:12 -0700

    Like, comment, subscribe

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.c"
-/*
- * _abs-A function that computes the absolute value of a number
- * @c: The character to print
- * Return:Absolute value of ab
- */
+/**
+*_abs-A function that computes the absolute value of a number
+*@c: The character to print
+*Return:Absolute value of ab
+*/
 int _abs(int)
 {
-	return (ab * ((ab > 0) - (ab < 0)));
+	return (ab * (ab > 0) - (ab < 0));
 }
 
