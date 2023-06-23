fd906131d9b164e9e4a0d73d523fc2d519240db2
Abraham <abrahamjms156@gmail.com>
2023-06-22 19:04:50 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers-prints more numbers ten times
+ *followed by a new line
+Return:(0)
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar( / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putcar(j % 10 + '0');
+	}
+	_putchar('\n');
+}
