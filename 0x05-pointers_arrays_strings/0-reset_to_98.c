1606e01bf122ed187eefd34555a46b463643e8ee
Abraham <abrahamjms156@gmail.com>
2023-06-26 07:39:04 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *reset_to_98-a program that takes a pointer to int
+ *and updates its value to 98
+ *@n:integer
+ *Return:(n)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
