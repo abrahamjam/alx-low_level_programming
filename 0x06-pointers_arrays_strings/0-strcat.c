6876675caeb87f53cb6998f893414d002f89ad2b
Abraham <abrahamjms156@gmail.com>
2023-06-29 11:30:03 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcat-concatenate strings
+ *@dest:input value
+ *@src:input value
+ *Return:void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+		j++;
+	}       dest[i] = '\0';
+	return (dest);
+}
