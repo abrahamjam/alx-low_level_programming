0b0d20eea35aaa2c3b35f99f6f87425392900792
Abraham <abrahamjms156@gmail.com>
2023-06-29 14:22:35 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *string_toupper-changes all lower caes to upper
+ *@n:pointer
+ *Return:n
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+
+		}
+		i++;
+
+	}
+	return (n);
+}
