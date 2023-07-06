15896b5a5769d46d8cd692d53fc1448f9e390eee
Abraham <abrahamjms156@gmail.com>
2023-07-05 19:45:41 -0700

    Inception. Is it possible?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+/**
+ *_sqrt_recursion-returns the natural square root of a number
+ *@n:number
+ *Return:natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return(_sqrt(n, 1));
+}
+
 /**
  *sqrt-prints square root of integers
  *@n:number
  *@i:iterate number
  *Return:integer
  */
-int _sqrt(int n, int i)
+int _sqrt_recursion(int n, int i)
 {
 	int sqrt = i * i;
 
