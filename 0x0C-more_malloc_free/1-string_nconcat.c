ba8d025ff26b049ace4dc8508274a5c7a3fa68f3
Abraham <abrahamjms156@gmail.com>
2023-07-13 14:30:42 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,10 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0; l = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int l = 0;
+	unsigned int k = 0;
 	char *str;
 
 	if (s1 == NULL)
