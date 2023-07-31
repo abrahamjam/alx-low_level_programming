ad8ccce8853cb662549ed7ffa094f0c7daee689b
abrahamjam <abrahamjames156@gmail.com>
2023-07-31 13:47:40 -0700

    Find the loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  *free_listint_safe-frees a linked list
- *@h:pointer to the first node in the linked list
+ *@h:pointer to the first node in the linked lists
  *Return:number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
@@ -29,7 +29,7 @@ size_t free_listint_safe(listint_t **h)
 			len++;
 			break;
 		}
-		*h = NULL;
 	}
+	*h = NULL;
 	return (len);
 }
