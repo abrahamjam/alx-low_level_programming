41c3968d7d70d3132d2e77cf102f4fcac5007ef3
MAC <mac@MACs-MacBook-Pro.local>
2023-09-10 18:07:39 +0100

    converts binary number to unsigned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *binary_to_uint-converts a binary number to an
+ *unsigned int.
+ *@b:binary.
+ *Return:unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int u;
+	int leng, base_two;
+
+	if (!b)
+		return (0);
+	u = 0;
+	for (leng = 0; b[leng] != '\0'; leng++)
+		;
+	for (leng--, base_two = 1; leng >= 0; leng--, base_two *= 2)
+	{
+		if (b[leng] != '0' && b[leng] != '1')
+		{
+			return (0);
+		}
+
+		if (b[leng] & 1)
+		{
+			u += base_two;
+		}
+	}
+	return (u);
+}
