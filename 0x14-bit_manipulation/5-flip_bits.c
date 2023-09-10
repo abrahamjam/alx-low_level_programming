93e003971afd569081865decb71434597b6fd5f5
MAC <mac@MACs-MacBook-Pro.local>
2023-09-10 18:35:52 +0100

    101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *flip_bits-returns the number of bits you would
+ *need to flip to get from one number to another
+ *@n:number one.
+ *@m:number two.
+ *Return:number of bits.
+ */
+unsigned int flip_bits(unsigned long int x, unsigned long int y)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; x || y; x >>= 1, y >>= 1)
+	{
+		if ((x & 1) != (y & 1))
+			nbits++;
+	}
+	return (nbits);
+}
