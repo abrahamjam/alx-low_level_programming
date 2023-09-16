6018cdf5bb733044efbaa962d945bda40be08d19
MAC <mac@MACs-MacBook-Pro.local>
2023-09-16 19:46:54 +0100

    Tread lightly,she might hear

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,29 @@
 #include "main.h"
-#include "stdlib.h>
+
 /**
- *read_textfile-read file and prints to STDOUT
- *@filename:file to be read
- *@letters:number of bytes read
- *Return:w(actual num of bytes read and printed),0 when funct fails or filename  *is NULL
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters:letters printed.
+ * Return:no of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-     char *buf;
-     ssize_t fd;
-     ssize_t w;
-     ssize_t t;
-     
-     fd = open(filename, O_RDONLY);
-     if (fd == -1)
-          return (0);
-     buf = malloc(sizeof(char) * letters);
-     t = read(fd, buf, letters);
-     w = write(STDOUT_FILENO, buf, t);
-     free(buf);
-     close(fd);
-     close(fd);
-     return (w);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+	close(fd);
+	free(buf);
+	return (nwr);
 }
