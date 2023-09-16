83040cc60f47264e0cbcecc7129f28d6e0ff4c96
MAC <mac@MACs-MacBook-Pro.local>
2023-09-16 20:04:28 +0100

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,25 +1,31 @@
 #include "main.h"
+
 /**
- *create_file-files
- *@filename:pointer to file
- *@text_content:pointer to string
- *Return:-1 if failure, otherwise 1
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-     int fl, w, len = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-     if (filename == NULL)
-          return (-1);
-     if (text_content !- NULL)
-     {
-          for (len = 0; text_content[len];)
-               len++;
-     }
-     fl = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-     w = write(fl, text_content, len);
-     if (fl == -1 || w == -1)
-          return (-1);
-     close(fl);
-     return (1);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,32 @@
 #include "main.h"
+
 /**
- *append_text_to_file-Appends text at end of file.
- *@filename:A pointer to the name of the file.
- *@text_content:string to add to the end of the file.
- *Return:If function fails or filename is NULL - -1.
- *If file does not exist user lacks write permissions - -1.
- *Otherwise - 1.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op, wr, leng = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content)
 	{
-		for (leng = 0; text_content[len];)
-			len++;
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+		if (rwr == -1)
+			return (-1);
 	}
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, leng);
-
-	if (op == -1 || wr == -1)
-		return (-1);
-	close(op);
+	close(fd);
 	return (1);
 }                
